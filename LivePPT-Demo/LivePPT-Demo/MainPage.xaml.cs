63b2ed7be9c57faecfd575c00b7e2a3a4e4355ee
mcbaizi <ggggmcbaizi@live.com>
2013-08-02 16:39:10 +0800

    常见的view，页面导航，button，图片显示

---
+++ LivePPT-Demo/LivePPT-Demo/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace LivePPT_Demo
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // 构造函数
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // 将 listbox 控件的数据上下文设置为示例数据
+            DataContext = App.ViewModel;
+            this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+        }
+
+        // 为 ViewModel 项加载数据
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (!App.ViewModel.IsDataLoaded)
+            {
+                App.ViewModel.LoadData();
+            }
+        }
+
+        private void StartCof_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("/ViewImages.xaml", UriKind.Relative));
+        }
+    }
+}
---
+++ LivePPT-Demo/LivePPT-Demo/ViewImages.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.Windows.Media.Imaging;
+
+namespace LivePPT_Demo
+{
+    public partial class ViewImages : PhoneApplicationPage
+    {
+        public ViewImages()
+        {
+            InitializeComponent();
+        }
+
+        private void changeImage_Click(object sender, RoutedEventArgs e)
+        {
+            BitmapImage imageInbox = new BitmapImage(new Uri("/LivePPT-Demo;component/Images/Lighthouse.jpg", UriKind.Relative));
+            imageBox.Source = imageInbox; 
+        }
+    }
+}
